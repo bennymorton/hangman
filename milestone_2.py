9a229c8e4cee2ec28a0d8667a800343e3446d06e
Ben <benjaminmorton@live.co.uk>
2022-12-09 15:43:24 +0000

    User input functionality and basic error handling added

--- milestone_2.py
+++ milestone_2.py
@@ -3,4 +3,9 @@ import random
 word_list = ['bananas', 'blueberries', 'apples', 'pears', 'raspberries']
 word = random.choice(word_list)
 
-print(word)
+guess = input('Guess a letter \n')
+
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
