24af018b9097e9d85e2c9d0db08bd33df66e3758
Ben <benjaminmorton@live.co.uk>
2022-12-12 11:30:33 +0000

    Class initialisation updated

--- milestone_4.py
+++ milestone_4.py
@@ -1,16 +1,18 @@
 import random
 
+
+
 class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
 
-        word = random.choice(self.word_list)
+        self.word = random.choice(self.word_list)
 
-        word_guessed = []
-        for i in range(len(word)):
-            word_guessed = word_guessed.append('_')
+        self.word_guessed = []
+        for i in range(len(self.word)):
+            self.word_guessed = self.word_guessed.append('_')
         
-        num_letters = len(set((letter for letter in word)))
+        self.num_letters = len(set((letter for letter in self.word)))
 
-        list_of_guesses = []
+        self.list_of_guesses = []
