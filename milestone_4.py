b4a68b0c74217516da991c07270682dc918dae0b
Ben <benjaminmorton@live.co.uk>
2022-12-12 12:18:24 +0000

    check_guess and ask_for_input methods added to class

--- milestone_4.py
+++ milestone_4.py
@@ -6,20 +6,33 @@ class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
-
         self.word = random.choice(self.word_list)
-
         self.word_guessed = []
         for _ in range(len(self.word)):
             self.word_guessed.append('_')
 
         self.num_letters = len(set(self.word))
-
         self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print('Good guess! {} is in the word.'.format(guess))
+        
+    def ask_for_input(self):
+        while True:
+            guess = input('Guess a letter ')
+            
+            if len(guess) != 1 or not guess.isalpha():
+                print('Invalid letter. Please, enter a single alphabetical character.')
+            elif guess in self.list_of_guesses:
+                print('You already tried that letter!')
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
 
 test = Hangman(word_list)
 
 print(test.word)
-print(test.word_guessed)
-print(test.num_letters)
-print(test.list_of_guesses)
+
+test.ask_for_input()
